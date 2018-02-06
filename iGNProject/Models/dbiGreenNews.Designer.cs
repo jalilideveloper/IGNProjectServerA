5666743d170d577786c86ec7fed822fe137409d2
jalili.developer@gmail.com <jalili.developer@gmail.com>
2018-02-06 08:04:47 -0800

    asdsa

--- iGNProject/Models/dbiGreenNews.Designer.cs
+++ iGNProject/Models/dbiGreenNews.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\ProjectAM\iGreenNews\iGNProject\iGNProject\Models\dbiGreenNews.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\ProjectAM\IGNProjectServerA\iGNProject\Models\dbiGreenNews.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
